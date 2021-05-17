5c701180f1a051147b3dd66fa049fb907bc9fac3
Shahzod9941 <shahzodtoatov@gmail.com>
2021-05-17 19:20:30 +0500

    initial version

--- js/app.js
+++ js/app.js
@@ -1,7 +1,71 @@
 
-const modal__swiper = new Swiper('.modal__swiper', {
+ const modal__swiper = new Swiper('.modal__swiper', {
     // Optional parameters
-    loop: true,
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+        200: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        }        
+    }
+  });
+
+  const modal__swiper2 = new Swiper('.modal__swiper2', {
+    // Optional parameters
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+        200: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        }        
+    }
+  });
+
+  const modal__swiper3 = new Swiper('.modal__swiper3', {
+    // Optional parameters
+    loop: false,
   
     // If we need pagination
     pagination: {
@@ -36,3 +100,4 @@ const modal__swiper = new Swiper('.modal__swiper', {
     $('.modal').on('shown.bs.modal', function(e) {
         modal__swiper.update();
       });
+      
